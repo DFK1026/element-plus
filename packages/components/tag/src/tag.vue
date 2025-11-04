a884a6c0283a4c3e5c57865af26219550fa50c2a
王棁 <2390357648@qq.com>
2025-11-04 12:52:04 +0800

    Update tag.vue to use disableTransitions
    Refactor tag component to use disableTransitions prop and remove inTable logic.

--- packages/components/tag/src/tag.vue
+++ packages/components/tag/src/tag.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    v-if="shouldDisable"
+    v-if="disableTransitions"
     :class="containerKls"
     :style="{ backgroundColor: color }"
     @click="handleClick"
@@ -34,7 +34,7 @@
 </template>
 
 <script lang="ts" setup>
-import { computed, onMounted, ref, getCurrentInstance } from 'vue'
+import { computed } from 'vue'
 import ElIcon from '@element-plus/components/icon'
 import { Close } from '@element-plus/icons-vue'
 import { useNamespace } from '@element-plus/hooks'
@@ -80,21 +80,4 @@ const handleVNodeMounted = (vnode: VNode) => {
     vnode.component.subTree.component.bum = null
   }
 }
-
-const inTable = ref(false)
-onMounted(() => {
-  try {
-    const instance: any = getCurrentInstance()
-    const el = instance?.proxy?.$el
-    if (el && typeof el.closest === 'function') {
-      inTable.value = !!el.closest('.el-table')
-    }
-  } catch (e) {
-    // ignore
-  }
-})
-
-const shouldDisable = computed(() => {
-  return !!props.disableTransitions || inTable.value
-})
 </script>
